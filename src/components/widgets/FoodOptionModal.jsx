15fcb17803acf3e1aae0a932004f9e008d2f366b
thawatchaiUdjan <thawatchai_udjan@outlook.com>
2024-08-30 13:26:13 +0700

    [UPDATE] add food option choices price on click buy food

--- src/components/widgets/FoodOptionModal.jsx
+++ src/components/widgets/FoodOptionModal.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import FoodCartContext from '../contexts/FoodCartContext'
 import { useForm } from 'react-hook-form'
 import ButtonIcon from './ButtonIcon'
@@ -6,14 +6,17 @@ import { ShoppingCartIcon } from '@heroicons/react/24/solid'
 import InputTextArea from './InputTextArea'
 
 export default function FoodOptionModal({ food, isShow, close }) {
-    const { register, handleSubmit, reset, formState: { errors } } = useForm()
+    const { register, handleSubmit, reset, watch, formState: { errors } } = useForm()
     const { addFoodCart } = useContext(FoodCartContext)
+    const [totalPrice, setTotalPrice] = useState(food.food_price)
+    const allOptions = watch()
 
     function onSubmit(data) {
         const foodOptions = {
             option_string: getStringOptions(data),
             option_note: data['option-note']
         }
+        food.food_price = totalPrice
         addFoodCart({ food, foodOptions })
         close()
     }
@@ -23,18 +26,45 @@ export default function FoodOptionModal({ food, isShow, close }) {
             const option = data[key]
             if (key != 'option-note' && option) {
                 if (Array.isArray(option)) {
-                    acc.push(...option)
+                    option.forEach((option) => {
+                        const choice = JSON.parse(option)
+                        acc.push(choice.choice_name)
+                    })
                 } else {
-                    acc.push(option)
+                    const choice = JSON.parse(option)
+                    acc.push(choice.choice_name)
                 }
             }
             return acc
         }, [])
     }
 
+    function calculateTotalPrice() {
+        let foodPrice = parseFloat(food.food_price)
+        Object.keys(allOptions).reduce((acc, key) => {
+            const option = allOptions[key]
+            if (key != 'option-note' && option) {
+                if (Array.isArray(option)) {
+                    option.forEach((option) => {
+                        const choice = JSON.parse(option)
+                        foodPrice += choice.choice_price
+                    })
+                } else {
+                    const choice = JSON.parse(option)
+                    foodPrice += choice.choice_price
+                }
+            }
+        }, [])
+        setTotalPrice(foodPrice.toFixed(2))
+    }
+
     useEffect(() => {
         reset()
     }, [food])
+
+    useEffect(() => {
+        calculateTotalPrice()
+    }, [allOptions])
 
     return (
         <dialog id='food-option-modal' className={`${isShow ? 'modal-open' : 'modal-closed'} modal z-10 max-w-full`}>
@@ -51,7 +81,7 @@ export default function FoodOptionModal({ food, isShow, close }) {
                                 className="object-cover w-full h-full border border-gray-300 rounded-xl"
                             />
                         </div>
-                        <div className='flex items-baseline mt-3 mb-1 self-start'>
+                        <div className='flex flex-wrap items-baseline mt-3 mb-1 self-start'>
                             <span className='text-3xl text-primary truncate mr-3'>{food.food_name}</span>
                             <span className="text-primary text-2xl text-opacity-70">{food.food_price}$</span>
                             <span className="text-sm text-gray-400 line-through ml-2">{food.food_price_discount}$</span>
@@ -93,7 +123,7 @@ export default function FoodOptionModal({ food, isShow, close }) {
                     <div className="modal-action">
                         <ButtonIcon
                             type={'submit'}
-                            text={'Add to cart'}
+                            text={`Add to cart - $${totalPrice}`}
                             icon={<ShoppingCartIcon className='size-6' />}>
                         </ButtonIcon>
                     </div>
@@ -104,6 +134,7 @@ export default function FoodOptionModal({ food, isShow, close }) {
     )
 
     function SelectOption({ option, name, register, rules, errors }) {
+        const watchOptions = watch(name) || []
         return (
             <div>
                 <div className='mb-3 mt-5'>
@@ -112,14 +143,15 @@ export default function FoodOptionModal({ food, isShow, close }) {
                 </div>
                 {
                     option.option_choices.map((choice, index) => (
-                        <label key={index} className='flex gap-3 mb-3 cursor-pointer'>
+                        <label key={index} className={`flex items-center gap-3 mb-3 cursor-pointer ${watchOptions.includes(JSON.stringify(choice)) ? 'font-semibold' : ''}`}>
                             <input
                                 type='radio'
-                                className={`radio ${errors[name] ? 'radio-error' : 'radio-primary'} `}
-                                value={choice.choice_name}
+                                className={`radio ${errors[name] ? 'radio-error' : 'radio-primary'}`}
+                                value={JSON.stringify(choice)}
                                 {...register(name, rules)}
                             />
-                            <span className={`${errors[name] ? 'text-error' : ''}`}>{choice.choice_name}</span>
+                            <span className={`${errors[name] ? 'text-error' : ''} flex-1`}>{choice.choice_name}</span>
+                            <span className={`${errors[name] ? 'text-error' : ''} text-md text-gray-500`}>{choice.choice_price > 0 ? `+$${choice.choice_price}` : ''}</span>
                         </label>
                     ))
                 }
@@ -128,6 +160,7 @@ export default function FoodOptionModal({ food, isShow, close }) {
     }
 
     function CheckBoxOption({ option, name, register, rules, errors }) {
+        const watchOptions = watch(name) || []
         return (
             <div>
                 <div className='mb-3 mt-5'>
@@ -136,14 +169,15 @@ export default function FoodOptionModal({ food, isShow, close }) {
                 </div>
                 {
                     option.option_choices.map((choice, index) => (
-                        <label key={index} className='flex gap-3 mb-3 cursor-pointer'>
+                        <label key={index} className={`flex items-center gap-3 mb-3 cursor-pointer ${watchOptions.includes(JSON.stringify(choice)) ? 'font-semibold' : ''}`}>
                             <input
-                                className={`checkbox ${errors[name] ? 'checkbox-error' : 'checkbox-primary'} `}
+                                className={`checkbox ${errors[name] ? 'checkbox-error' : 'checkbox-primary'}`}
                                 type='checkbox'
-                                value={choice.choice_name}
+                                value={JSON.stringify(choice)}
                                 {...register(name, rules)}
                             />
-                            <span className={`${errors[name] ? 'text-error' : ''}`}>{choice.choice_name}</span>
+                            <span className={`${errors[name] ? 'text-error' : ''} flex-1`}>{choice.choice_name}</span>
+                            <span className={`${errors[name] ? 'text-error' : ''} text-md text-gray-500`}>{choice.choice_price > 0 ? `+ $${choice.choice_price}` : ''}</span>
                         </label>
                     ))
                 }

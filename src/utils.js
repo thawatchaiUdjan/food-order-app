ea0c1486b6acb00152bddce616af83153eb723ea
thawatchaiUdjan <thawatchai_udjan@outlook.com>
2024-09-09 20:07:35 +0700

    [FIX] when append array of form data, alway treat as array

--- src/utils.js
+++ src/utils.js
@@ -16,7 +16,7 @@ export function getCombineFormData(data) {
   const formData = new FormData()
   Object.keys(data).forEach(key => {
     if (Array.isArray(data[key])) {
-      data[key].forEach(item => formData.append(key, item))
+      data[key].forEach(item => formData.append(`${key}[]`, item))
     } else {
       formData.append(key, data[key])
     }
